e378fc6216647b0326368481f813d547e997585d
snow <rhasya@gmail.com>
2024-10-07 15:40:27 +0900

    Fix errata

--- src/utils/message.js
+++ src/utils/message.js
@@ -2,4 +2,4 @@ export const CREATE_MESSAGE = 'Task has been registered';
 export const UPDATE_MESSAGE = 'Task has been modified';
 export const COMPLETE_MESSAGE = 'Task has been completed';
 export const DELETE_MESSAGE = 'Task has been deleted';
-export const DELETE_ALL_MESSAGE = '“All tasks have been deleted';
+export const DELETE_ALL_MESSAGE = 'All tasks have been deleted';
